7a1df9e1193a70309697edbefa2565dbad08e91f
Lucas Vinicius <lucas.anubis@hotmail.com.br>
2026-06-10 14:06:49 -0300

    Conectado PostgreSQL ao backend

--- Backend/src/server.ts
+++ Backend/src/server.ts
@@ -1,11 +1,23 @@
 import express from "express";
+import { pool } from "./database/connection";
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.json({
-    message: "FiscalFlow API Online"
-  });
+app.get("/", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT NOW()");
+
+    res.json({
+      message: "FiscalFlow API Online",
+      database: "Conectado",
+      horarioBanco: result.rows[0],
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Erro ao conectar ao banco",
+      error,
+    });
+  }
 });
 
 const PORT = 3000;
